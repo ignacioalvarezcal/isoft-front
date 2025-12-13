295246c174f46a09271039b9b73a5c2ed2ca78e0
Ignacio Alvarez <ign.alvarezcal@gmail.com>
2025-12-13 01:22:30 -0300

    Primer commit: proyecto Astro listo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/static'; // o '@astrojs/vercel/serverless' si usas SSR
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server', // ← Solo si estás usando SSR en alguna página
+  adapter: vercel(),
+});
